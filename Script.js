9efd6440f783b94b4b93623f55a8fd575f14aa74
Jyoti-Parashar <parashar.jyoti81@gmail.com>
2024-09-17 11:11:46 -0400

    CRD Chamges

--- Script.js
+++ Script.js
@@ -11,30 +11,154 @@
  fetch(" https://66c61941134eb8f43496bf4b.mockapi.io")
  fetch("https://64407795792fe886a88f6162.mockapi.io/api/todos")  //todo api
  */
- 
+
+ const endpoinUrl="http://localhost:3000/tasks"
+
+let tasks = []
+let createdAt = new Date();
+let lastId=0;
+let lastCreatedItem=null;
+
+//Get all todo from server
+async function getTasks() {
+ try {
+   //Use fetch api for response
+   const response = await fetch(endpoinUrl);
+   if (response.status!=200) {
+     throw new Error(`Some Error, Status Code:${response.status}`);
+   }
+   const data = await response.json();
+   // console.log(data);
+   tasks = data.reverse();
+  // console.log(tasks);
+  for (const task of tasks) {
+    lastId=task.id;
+  }
 
 
-function getTasks(){
+  
+  // lastId=tasks[tasks.length-1].id;
+  // console.log(`lastId : ${lastId}`);
+   renderTasks();
+  
+ } catch (error) {
+    console.log(error);
+ }
+};
 
-   //create Base URL variable
-   //const BASE_URL = "http://localhost:3000";
-   const BASE_URL="https://66c61941134eb8f43496bf4b.mockapi.io"
 
-   /**API Request Functions */
- 
-   //get all todos from DB
-   const fetchTodos = async () => {
-     //fetch data from the server using the fetch API
-     const response = await fetch(`${BASE_URL}/tasks`);
- 
-     //convert the response to JSON
-     const data = await response.json();
-      console.log({ data });
- 
-     //return the data
-     return data;
-   };
+//add a new todo to the server
+const addTodo = async (text) => {
+  
+  console.log("addTodo taking in : " , text)
+  //fetch data from the server using the fetch API
+  const response = await fetch('http://localhost:3000/tasks', {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({title: text, completed: false, createdAt }),
+  });
 
- console.log(fetchTodos);
+  //convert the response to JSON
+  const data = await response.json();
+  // console.log({ data });
+  getTasks()
+  //return the data
+  return data;
+};
+
+
+
+
+//update to do
+
+
+const updateTodo = async (text) => {
+  //fetch data from the server using the fetch API
+  const response = await fetch('http://localhost:3000/tasks', {
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({  title:text, completed: false, createdAt }),
+  });
+
+  //convert the response to JSON
+  const data = await response.json();
+  // console.log({ data });
+
+  //return the data
+  return data;
+};
+
+
+//add event listener to the add todo button
+$("#addTodo").on("click", async () => {
+  
+  //get the value of the input field
+  const text = $("#newTask").val();
+   console.log(text);
+
+  if (!text) {
+    alert("Please enter a todo");
+    return;
+  }
+ //addTodo( text);
+  //add the todo to the server
+  try {
+    await addTodo( text);
+   
+  } catch (error) {
+    console.log(error);
+  } finally {
+    //clear the input field regardless of the outcome
+    $("#newTask").val("");
+  } 
+});
+
+async function renderTasks() {
+  const showTable = document.getElementById("showTable")
+  showTable.innerHTML = "";
+  for (const task of tasks) {
+    console.log(task)
+    const row = document.createElement("tr")
+    row.innerHTML =
+      `
+        <td>${task.id}</td>
+        <td>${task.title}</td>
+        <td>${task.completed}</td>
+        <td>${task.createdAt}</td>
+        <td><button id="btn-Delete" class="btn btn-danger">Delete</button>
+        <button class="btn btn-warning" id="EditTask"> Edit </button></td>
+    `
+    row.querySelector("#btn-Delete").addEventListener("click", async () => {
+      const response = await fetch("http://localhost:3000/tasks/" + task.id, { method: "DELETE" });
+      const data = await response.json();
+
+
+      //update the state(short term memory)
+
+      const indexTodelete = tasks.indexOf(task);
+      tasks.splice(indexTodelete, 1);
+
+      // re-render
+   
+      renderTasks();
+
+
+    })
+    showTable.appendChild(row)
+   
+  }
+
 }
+
+
+
+//call the fetch function
+
+getTasks();
+
+
 
